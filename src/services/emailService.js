f69070f8b93910e8b38906568e0068741fff23c5
Tasin-Hossain <miniofficial51@gmail.com>
2026-04-10 15:17:21 +0600

    v3.

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,15 +5,12 @@ class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // must be false for 587
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000, // add this
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     });
   }
 
